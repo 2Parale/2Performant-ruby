8678e5716f198231279df4d8dcc0c1e724523a2b
Andrei Bocan <zmaxor@gmail.com>
2010-04-26 17:04:47 +0300

    More extensions

--- lib/two_performant/core_ext/string.rb
+++ lib/two_performant/core_ext/string.rb
@@ -6,4 +6,12 @@ class String
       tr("-", "_").
       downcase
   end
+
+  def camelize(first_letter_in_uppercase = true)
+    if first_letter_in_uppercase
+      self.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    else
+      self.first.downcase + camelize(self)[1..-1]
+    end
+  end
 end
