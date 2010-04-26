bc39c458b5d969f8cea3e62af2d20f2746332137
Andrei Bocan <zmaxor@gmail.com>
2010-04-26 19:08:43 +0300

    Cheesy initialization

--- lib/two_performant.rb
+++ lib/two_performant.rb
@@ -7,8 +7,18 @@ require 'two_performant/core_ext/string'
 require 'two_performant/resource'
 
 module TwoPerformant
+  @@site = nil
+
   def self.logger
     @logger ||= Logger.new STDOUT
+  end
+
+  def self.site
+    @@site
+  end
+
+  def self.site=(value)
+    @@site = value
   end
 
 end
